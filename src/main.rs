aa47598f483753cebd51e6be01554f4d0dcca2de
Шапошников Евгений <shaposhnikovea@lad24.ru>
2023-08-15 10:40:26 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,17 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
                 ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        },
-        _ => {}
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            }
+            _ => (),
+        }
     });
 }
