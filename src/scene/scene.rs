6dd239e37b85c99b60b99687637b470cfa648cd7
Eugene Shaposhnikov <eawapownikov@gmail.com>
2023-09-23 06:15:11 +0000

    refactor traced

--- src/scene/scene.rs
+++ src/scene/scene.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     gpu::{GPUVertex, GetGPUData},
@@ -11,21 +11,21 @@ use super::tracer::Tracer;
 #[derive(Clone, Debug)]
 pub struct Scene {
     pub tracer: Tracer,
-    objects: Vec<Object>,
+    objects: HashMap<String, Object>,
     traced: HashSet<String>,
 }
 
 impl Scene {
     pub fn new() -> Self {
         Scene {
-            objects: Vec::new(),
+            objects: HashMap::new(),
             tracer: Tracer::new(),
             traced: HashSet::new(),
         }
     }
 
     pub fn add(mut self, object: Object) -> Self {
-        self.objects.push(object.clone());
+        self.objects.insert(object.id.clone(), object.clone());
         self.tracer.index(object.id.clone(), object.get_mesh());
         self
     }
@@ -43,13 +43,13 @@ impl Scene {
         changed
     }
 
-    // fn get_object_by_id(&self) -> Option<Object> {
-    //     return
-    // }
+    fn get_object_by_id(&mut self, object_id: &String) -> Option<&mut Object> {
+        self.objects.get_mut(object_id)
+    }
 
     fn mark_traced(&mut self, traced_ids: &HashSet<String>, status: bool) {
         for id in traced_ids.iter() {
-            println!("{:?} {:?}", id, status);
+            self.get_object_by_id(id).unwrap().set_highlighted(status);
         }
     }
 }
@@ -57,7 +57,7 @@ impl Scene {
 impl GetGPUData for Scene {
     fn get_gpu_data(&self) -> Vec<GPUVertex> {
         let mut result: Vec<GPUVertex> = Vec::new();
-        for obj in self.objects.iter() {
+        for (_, obj) in self.objects.iter() {
             let data = obj.get_gpu_data();
             for item in data {
                 result.push(item)
