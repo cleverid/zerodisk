bf65046c35c432db7bf65cfa98daafd385744841
Шапошников Евгений <shaposhnikovea@lad24.ru>
2023-11-02 13:15:19 +0300

    Адд file

--- server/src/service/start.rs
+++ server/src/service/start.rs
@@ -1,9 +1,15 @@
 use chrono::Utc;
-use tarantool::proc;
+use tarantool::{proc, space::Space};
+use nanoid::nanoid;
 
 #[proc]
 fn start() -> Result<bool, String> {
     let start_time = Utc::now().time();
+    
+    let space = Space::find("file").expect("Can't find space plan_item");
+    let row = (nanoid!(), 0, "file.png");
+    space.replace(&row).expect("Error insert file");
+    
     let ms = (Utc::now().time() - start_time).num_milliseconds();
     println!("Time in ms: {:?}", ms);
     Ok(true)
